0b1639b9e198d3ef54b03094ee62ef443d42f75b
9Raghav <raghavpurohit.rp@gmail.com>
2025-05-26 22:30:44 -0700

    Update tesla_data.py

--- tesla_data.py
+++ tesla_data.py
@@ -1,3 +1,15 @@
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.io as pio
+pio.renderers.default = "iframe"
+import warnings
+# Ignore all warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
 def make_graph(stock_data, revenue_data, stock):
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
     stock_data_specific = stock_data[stock_data.Date <= '2021-06-14']
